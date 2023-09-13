e014cb508210222f83f9782ef41945b8dba3ff68
A-dead-pixel <me@otsrv.net>
2023-09-13 18:39:20 +0200

    Add talentctl for the update script and more

--- system/configuration.nix
+++ system/configuration.nix
@@ -26,14 +26,34 @@
 
   environment.systemPackages = with pkgs;
   let
-    update = writers.writeBashBin "update" ''
-      set -euo
-      if [[ $(id -u) -ne 0 ]]; then
-        exec su -c "''${BASH_SOURCE[0]}" || exit 1
+    update = writers.writeBashBin "talentctl" ''
+      set -e
+      if [[ "$1" == "clear" ]]; then
+        touch /home/user/clear_home
+        echo "A clear was scheduled, it will happen on the next boot."
+        exit 0
       fi
-      cd /etc/nixos
-      nix flake lock --update-input talentnix
-      nixos-rebuild switch
+      if [[ "$1" == "cancel_clear" ]]; then
+        if [[ -e /home/user/clear_home ]]; then
+          rm /home/user/clear_home || exit 2
+          echo "Clear canceled."
+          exit 0
+        else
+          echo "No clear was scheduled."
+          exit 3
+        fi
+      fi
+      if [[ "$1" == "update" ]]; then
+        if [[ $(id -u) -ne 0 ]]; then
+            exec su -c "''${BASH_SOURCE[0]} $@" || exit 1
+        fi
+        cd /etc/nixos
+        nix flake lock --update-input talentnix
+        nixos-rebuild switch
+        exit 0
+      fi
+      echo -e "Usage:\n   talentctl <subcommand>\n"
+      echo -e "Available subcommands:\n - update\n - clear\n - cancel_clear"
     '';
   in
   [
